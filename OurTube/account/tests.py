18d866a509806f16d263ba9bf7fff85b774df19b
krasnowsky <wojtek.ref2@gmail.com>
2023-02-12 17:17:14 +0100

    cleanup account tests

--- OurTube/account/tests.py
+++ OurTube/account/tests.py
@@ -1,8 +1,6 @@
-import json
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from django.test import TestCase
 from django.urls import reverse
 
 from .models import User
